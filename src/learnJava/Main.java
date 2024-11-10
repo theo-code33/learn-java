15764fb0944a37f5a05a299bf4d2874eaa121a5c
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-11-10 11:46:32 +0100

    :sparkles: Create method addition

--- src/learnJava/Main.java
+++ src/learnJava/Main.java
@@ -2,11 +2,15 @@ package learnJava;
 
 public class Main {
     public static void main (String[] args) {
-        String text = "Hello World";
-        System.out.println(text);
+        int somme = Main.addition(2, 7);
+        System.out.println(somme);
     }
 
     public static void affiche(String messageToPrint) {
         System.out.println(messageToPrint);
+    }
+
+    public static int addition(final int number1, final int number2) {
+        return number1 + number2;
     }
 }
