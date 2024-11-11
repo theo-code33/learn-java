62aa65bcf94cbdb0aeec18fecef343d28584d11f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-11-11 15:57:50 +0100

    :recycle: change the accessibility of Bloc class

--- src/learnJava/Bloc.java
+++ src/learnJava/Bloc.java
@@ -1,9 +1,9 @@
 package learnJava;
 
-public class Bloc {
-    private int length;
-    private int width;
-    private int height;
+public abstract class Bloc {
+    protected int length;
+    protected int width;
+    protected int height;
 
     Bloc(final int length, final int width, final int height) {
         this.length = length;
