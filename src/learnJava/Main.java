6cc23b1aa65352d9dd8ee8aa93e568776554a7ec
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2024-11-11 14:44:59 +0100

    :sparkles: Create method zeroCounter

--- src/learnJava/Main.java
+++ src/learnJava/Main.java
@@ -1,8 +1,23 @@
 package learnJava;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main (String[] args) {
         Main.affichage("Marty");
+        List<Integer> numbers = Arrays.asList(1220, 1452, 1445, 0, 5874, 0, 3);
+        Main.zeroCounter(numbers);
+    }
+
+    public static void zeroCounter(List<Integer> numbers) {
+        Integer countOfZero = 0;
+        for(Integer number: numbers) {
+            if(number == 0) {
+                countOfZero++;
+            }
+        }
+        System.out.println(countOfZero);
     }
 
     public static void affichage(String messageToPrint) {
